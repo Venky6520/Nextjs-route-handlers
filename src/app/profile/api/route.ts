8b7f66e5c7047ae98f551f33a4115c27b79049be
Venkatesh kumar <167079967+Venky6520@users.noreply.github.com>
2025-03-19 18:30:29 +0530

    dynamic route handlers,GET,POST,PATCH,DELETE,Headers in route handlers,URL Query parameters

---
+++ src/app/profile/api/route.ts
@@ -0,0 +1,26 @@
+import { headers, cookies } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest){
+    // const requestHeaders=new Headers(request.headers);
+    // console.log(requestHeaders.get("Authorization"));
+    
+    const headerList= await headers()
+    console.log(headerList.get("Authorization"));
+
+    const theme = request.cookies.get("theme");
+    console.log(theme);
+    
+    const cookieStore= await cookies()
+    cookieStore.set("resultsPerPage","20")
+    console.log(cookieStore.get("resultsPerPage"));
+    
+    return new Response("<h1>Profile API Data</h1>",
+        {
+        headers:{
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=dark"
+        }
+    }
+    )
+}
